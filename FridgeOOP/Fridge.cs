aa61ee154550c491280ce47d6436dcdfb12c12a4
JHarrell1211 <julian1211@gmail.com>
2018-01-31 14:44:11 -0500

    added constructors and first method

--- FridgeOOP/Fridge.cs
+++ FridgeOOP/Fridge.cs
@@ -6,7 +6,72 @@ using System.Threading.Tasks;
 
 namespace FridgeOOP
 {
-    class Fridge
+    public class Fridge
     {
+        //fields
+        private bool isClean;
+        private string dispenserType;
+        private int foodAmount;
+        private bool bulbsWork;
+
+        //Properties
+        public bool IsClean
+        {
+            get { return this.isClean; }
+            set { this.isClean = value; }
+        }
+
+        public string DispenserType
+        {
+            get { return this.dispenserType; }
+        }
+
+        public int FoodAmount
+        {
+            get { return this.foodAmount; }
+            set { this.foodAmount = value; }
+        }
+
+        public bool BulbsWork
+        {
+            get { return this.bulbsWork; }
+            set { this.bulbsWork = value; }
+        }
+
+        //Constructors
+        public Fridge()
+        {
+            this.dispenserType = "basic water dispenser";
+        }
+
+        public Fridge(string dispenserType)
+        {
+            this.dispenserType = dispenserType;
+        }
+
+        public Fridge(bool isClean, string dispenserType, int foodAmount, bool bulbsWork)
+        {
+            this.isClean = isClean;
+            this.dispenserType = dispenserType;
+            this.foodAmount = foodAmount;
+            this.bulbsWork = bulbsWork;
+        }
+        //Methods()
+        public void DispenseWater()
+        {
+            if (dispenserType == "basic water dispenser")
+            {
+                Console.WriteLine("Here's some lukewarm water");
+            }
+            else if(dispenserType == "deluxe dispenser")
+            {
+                Console.WriteLine("Enjoy your perfect ice water");
+            }
+            else
+            {
+                Console.WriteLine("What's gonna come out? It's a surprise!");
+            }
+        }
+
     }
 }
--- FridgeOOP/Program.cs
+++ FridgeOOP/Program.cs
@@ -10,6 +10,8 @@ namespace FridgeOOP
     {
         static void Main(string[] args)
         {
+            Fridge fridgeOne = new Fridge();
+            fridgeOne.DispenseWater();
         }
     }
 }
